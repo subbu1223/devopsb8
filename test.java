2953c4eae88cba5d58c20aa3103e89a34c5e62ae
SRTvenkat <gujjula.subbareddy@gmail.com>
2021-06-29 08:30:44 +0530

    offline commit

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello gist @ GitHub!");   
+         System.out.println("Hello gist @ GitHub!");
+	 System.out.println("venkat devops trainer");
       }
 } 
